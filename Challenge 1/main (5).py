7eb98b02abc138cbf3607afb7a1a43b4d5e44422
Swahja <147832100+Swahja@users.noreply.github.com>
2023-10-13 20:24:47 +0530

    Add files via upload

---
+++ Challenge 1/main (5).py
@@ -0,0 +1,27 @@
+class Student:
+  def __init__(self, name, roll_number, cgpa):
+      self.name = name
+      self.roll_number = roll_number
+      self.cgpa = cgpa
+
+def sort_students(student_list):
+  # Use the sorted function to sort the student_list based on cgpa in descending order
+  sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+  return sorted_students
+
+# Example usage:
+if __name__ == "__main__":
+  # Create a list of student objects
+  students = [
+      Student("Alice", "101", 3.9),
+      Student("Bob", "102", 3.5),
+      Student("Charlie", "103", 3.7),
+      Student("David", "104", 3.8),
+  ]
+
+  # Sort the list of students by CGPA in descending order
+  sorted_students = sort_students(students)
+
+  # Print the sorted list
+  for student in sorted_students:
+      print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
